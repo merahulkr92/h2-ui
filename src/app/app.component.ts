9116837fd12569b6eafd9e82d02e03b25764ef69
unknown <Administrator@INN14U-RT074.CORP.CYIENT.COM>
2020-06-24 18:24:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'h2-ui';
+  description= "this is test project"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
+import { FormsModule } from '@angular/forms';
+import { CustomerService } from './customer.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCustomerComponent,
+    CustomerDetailsComponent,
+    CustomersListComponent,
+    SearchCustomersComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
